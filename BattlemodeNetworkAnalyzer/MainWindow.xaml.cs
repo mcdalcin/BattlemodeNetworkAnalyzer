44c443d8814266fe1814d9c0e31fe703fb883890
Matthew Dalcin <matthew_dalcin@alumni.brown.edu>
2021-02-18 04:21:13 -0500

    Initial implementation.

--- BattlemodeNetworkAnalyzer/MainWindow.xaml.cs
+++ BattlemodeNetworkAnalyzer/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,21 +24,26 @@ namespace BattlemodeNetworkAnalyzer {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        class Packet {
+        class PacketInfo {
+            /** Time of sending/receiving packet. */
             public DateTime time;
 
-            public Packet(DateTime time) {
+            /** Length in bytes. */
+            public int length;
+
+            public PacketInfo(DateTime time, int length) {
                 this.time = time;
+                this.length = length;
             }
         }
 
         class IPInfo {
-            public List<Packet> outboundPackets = new List<Packet>();
-            public List<Packet> inboundPackets = new List<Packet>();
+            public List<PacketInfo> outboundPackets = new List<PacketInfo>();
+            public List<PacketInfo> inboundPackets = new List<PacketInfo>();
 
-            public System.Net.IPAddress destIp;
-            public IPInfo(System.Net.IPAddress destIp) {
-                this.destIp = destIp;
+            public string serverIp;
+            public IPInfo(string serverIp) {
+                this.serverIp = serverIp;
             }
         }
 
@@ -66,11 +72,36 @@ namespace BattlemodeNetworkAnalyzer {
         private CaptureDeviceInfo _captureDeviceInfo;
 
         /** IP Address to statistical information. */
-        private Dictionary<System.Net.IPAddress, IPInfo> addressToInfo;
-
+        private Dictionary<string, IPInfo> _addressToInfo = new Dictionary<string, IPInfo>();
+        private Object _addressToInfoLock = new Object();
+       
         public MainWindow() {
             InitializeComponent();
             InitializeDevices();
+            
+            new Thread(ShowStatistics).Start();
+        }
+
+        private void ShowStatistics() {
+            // Periodically update stats. This is an expensive operation as it locks
+            // the entire container. Therefore, we only do it every 5 seconds.
+            while (true) {
+                Application.Current.Dispatcher.Invoke(
+                    new Action(() => {
+                        lock (_addressToInfoLock) {
+
+                            cbIps.Items.Clear();
+                            foreach (string ip in _addressToInfo.Keys) {
+                                cbIps.Items.Add(ip);
+                            }
+                        }
+                    }));
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void RefreshInfo() {
+
         }
 
         private void InitializeDevices() {
@@ -108,7 +139,7 @@ namespace BattlemodeNetworkAnalyzer {
             // Trace.WriteLine("Listening on " + device.ToString() + "..." + device.Name + " ... " + device.MacAddress.ToString());
 
             device.Filter = "udp";
-            //device.StartCapture();
+            device.StartCapture();
         }
 
         private void StopCapturing(object sender, RoutedEventArgs e) {
@@ -120,25 +151,52 @@ namespace BattlemodeNetworkAnalyzer {
             _captureDeviceInfo = null;
         }
 
+        private void RefreshInfo(object sender, RoutedEventArgs e) {
 
+        }
 
-        private static void OnPacketArrival(object sender, CaptureEventArgs e) {
+        private void OnPacketArrival(object sender, CaptureEventArgs e) {
             var time = e.Packet.Timeval.Date;
-            var len = e.Packet.Data.Length;
+            var length = e.Packet.Data.Length;
             var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
             var udpPacket = packet.Extract<PacketDotNet.UdpPacket>();
             if (udpPacket != null) {
-
-                // 
                 var ipPacket = (PacketDotNet.IPPacket)udpPacket.ParentPacket;
-                System.Net.IPAddress srcIp = ipPacket.SourceAddress;
-                System.Net.IPAddress destIp = ipPacket.DestinationAddress;
+                IPAddress srcIp = ipPacket.SourceAddress;
+                IPAddress destIp = ipPacket.DestinationAddress;
                 
+                bool isInbound;
+                string serverIp;
+                // Verify that one of the addresses equals our capture device IP.
+                if (srcIp.ToString().Equals(_captureDeviceInfo.ipv4Address)) {
+                    isInbound = false;
+                    serverIp = destIp.ToString();
+                } else if (destIp.ToString().Equals(_captureDeviceInfo.ipv4Address)) {
+                    isInbound = true;
+                    serverIp = srcIp.ToString();
+                } else {
+                    // This should never happen, but if it does, let's report the error.
+                    Trace.WriteLine(
+                        "Neither src nor dest ip matches current device IP. " + 
+                        srcIp.ToString() + " " + destIp.ToString() + " " + 
+                        _captureDeviceInfo.ipv4Address);
+                    return;
+                }
                 // Look to see if we have encountered the IP already.
-                IPInfo info = 
+                lock (_addressToInfoLock) {
+                    IPInfo info =
+                        _addressToInfo.ContainsKey(serverIp)
+                            ? _addressToInfo[serverIp]
+                            : new IPInfo(serverIp);
 
-                
-                Trace.WriteLine(srcIp.ToString() + " : " + destIp.ToString() + " : " + ipPacket.Protocol.ToString());
+                    // Create the packet.
+                    PacketInfo packetInfo = new PacketInfo(time, length);
+                    if (isInbound) {
+                        info.inboundPackets.Add(packetInfo);
+                    } else {
+                        info.outboundPackets.Add(packetInfo);
+                    }
+                }
             }
         }
 
